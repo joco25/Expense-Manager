d42b34cdeed094e7a0efc8a03595493b06d4d212
joco25 <onungwej@gmail.com>
2018-02-24 15:39:19 +0100

    added edit and Remove actions for firebase

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,26 +1,42 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import {startAddExpense, addExpense, editExpense, removeExpense,setExpenses, startSetExpenses} from '../../actions/expenses';
+import {startAddExpense, addExpense, editExpense, startEditExpense, removeExpense,startRemoveExpense,setExpenses, startSetExpenses} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
-beforeEach((done)=> {
-    const data = [];
-    expenses.forEach(({ id, description,note, amount, createdAt})=>{
-        data[id]={description,note,amount,createdAt};
-    });
-    database.ref('expenses').set(data)
-            .then(()=> done())
-            .catch((e)=> console.log(e));
-});
+// beforeEach((done)=> {
+//     const data = [];
+//     expenses.forEach(({ id, description,note, amount, createdAt})=>{
+//         data[id]={description,note,amount,createdAt};
+//     });
+//     database.ref('expenses').set(data)
+//             .then(()=> done())
+//             .catch((e)=> console.log(e));
+// });
 
 test('should setup remove expense action object',()=>{
     const action = removeExpense({id: '123abc'});
     expect(action).toEqual({
         type: 'REMOVE_EXPENSE',
         id: '123abc'
+    })
+});
+
+test('should remove expense from firebase',(done)=>{
+    const store = createMockStore({});
+    const id = expenses[2].id;
+    store.dispatch(startRemoveExpense({id})).then(()=>{
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'REMOVE_EXPENSE',
+            id
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot)=>{
+        expect((snapshot.val())).toBeFAlsy();
+        done();
     })
 })
 
@@ -31,7 +47,25 @@ test('should setup edit expense action object',()=>{
         id: '123abc',
         updates: {name:'abc'}
     })
-})
+});
+
+test('should edit expense from firebase',()=>{
+    const store = createMockStore({});
+    const id = expenses[0];
+    const updates = {amount: 21845};
+    store.dispatch(startEditExpense(id,updates)).then(()=>{
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot)=>{
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+    });
+});
 
 test('should setup add expense action object with provided values',()=>{
     const action = addExpense(expenses[1]);
--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -3,16 +3,16 @@ import {shallow} from 'enzyme';
 import expenses from '../fixtures/expenses';
 import {EditExpensePage} from '../../components/EditExpensePage';
 
-let editExpense, removeExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(()=>{
-    editExpense = jest.fn();
-    removeExpense = jest.fn();
+    startEditExpense = jest.fn();
+    startRemoveExpense = jest.fn();
     history = {push: jest.fn()};
     wrapper = shallow(<EditExpensePage 
         expense={expenses[0]} 
-        editExpense={editExpense} 
-        removeExpense={removeExpense} 
+        startEditExpense={startEditExpense} 
+        startRemoveExpense={startRemoveExpense} 
         history={history}/>);
 });
 
@@ -20,14 +20,14 @@ test('should render EditExpensePage',()=>{
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should handle editExpense',()=>{
+test('should handle startEditExpense',()=>{
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(editExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
+    expect(startEditExpense).toHaveBeenLastCalledWith(expenses[0].id, expenses[0]);
 });
 
-test('should handle removeExpense',()=>{
+test('should handle startRemoveExpense',()=>{
     wrapper.find('button').simulate('click');
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(removeExpense).toHaveBeenLastCalledWith({id: expenses[0].id});
+    expect(startRemoveExpense).toHaveBeenLastCalledWith({id: expenses[0].id});
 });
